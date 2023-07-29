5834d8636a5bad5e6e89b125b8d873566b7d1678
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-07-28 23:47:13 -0300

    Criei o arquivo RetroShirts que é uma nova página onde terá as camisas Retrô (camisas sem ser do resto do mundo, de seleção ou de times brasileiros). Dentro desse arquivo vai buscar todas as camisas da API e fazer uma verificação na descrição de cada camisa para ver se encontra a palavra: retrô e não encontra as palavras seleção e rdm. Após isso, é mostrado todas as camisas Retrô da API na página. Depois adicionei a rota ao main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Account from './pages/Account';
 import BRShirts from './pages/BRShirts';
 import NationShirts from './pages/NationShirts';
 import RDMShirts from './pages/RDMShirts';
+import RetroShirts from './pages/RetroShirts';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -50,6 +51,10 @@ const router =  createBrowserRouter([
       {
         path: "/rdmshirts",
         element: <RDMShirts />
+      },
+      {
+        path: "/retroshirts",
+        element: <RetroShirts />
       },
     ] 
   },
