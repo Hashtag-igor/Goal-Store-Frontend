3db31bb9c8bb93ebca7344608f83d03bec032e0f
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-07-28 22:34:42 -0300

    Adicionei rotas ao main.jsx com o React Router Dom. Adicionei também o Outlet ao App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from "./pages/Home"
+import App from './App';
+import UserProvider from "./contexts/UserContext"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router =  createBrowserRouter([  
+  {
+    path: "/", 
+    element: <App />,                    
+    children: [                           
+      {
+        path: "/",                         
+        element: <Home />,
+      },
+    ] 
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
+  </React.StrictMode>
+);
