24c9cd26e8ed33178de545f34eade5a419b6f156
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-07-28 22:26:56 -0300

    Criei a Homepage. Dentro da Homepage eu consumo a minha API de camisas, verifico se tem algum usuario logado (através do useContext) e passo os dados que consumi da API pro component Card.jsx que acabei de criar. Ele será responsavel por criar um card com as informações passadas da API

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+
+export default function Card({name, description, price, img}) {
+  return (
+    <div style={{width: "300px", background: "green", height: "400px"}}>
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", width: "90%", margin: "auto"}}>
+            <img style={{width: "250px", height: "250px"}} src={img} alt="" />
+            <h3 style={{textAlign: "justify"}}>{name}</h3>
+            <p style={{textAlign: "justify"}}>{description}</p>
+            <p>Price: <strong>{price}</strong></p>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../contexts/UserContext';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Card from '../components/Card';
+
+export default function Home() {
+  const [product, setProduct] = useState([])
+  const { userLoggedIn } = useContext(UserContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const searchProducts = () => {
+    fetch("http://localhost:3001/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProduct(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    searchProducts(); // Chamada da função aqui
+
+    // Verificar se o usuário está logado
+    const isUserLoggedIn = userLoggedIn !== null;
+
+    if (!isUserLoggedIn && location.pathname !== '/') {
+      // Se o usuário não está logado e a rota atual não é '/', redirecioná-lo para a página de login
+      navigate('/login');
+    }
+  }, [userLoggedIn, location, navigate]);
+
+
+  return (
+    <div>
+      <div style={{backgroundImage: "url(https://www.superstore.com.br/images/lojas/foto-04-gr.jpg)", padding: "400px", backgroundPosition: "center", backgroundSize: "cover"}}>
+        <h1 style={{textAlign: "center", fontSize: "60px", color: "white", fontWeight: "bolder"}}>WELCOME,</h1>
+      </div>
+      <div style={{display: "flex", justifyContent: "space-evenly", flexWrap: "wrap", gap: "30px 0px", margin: "50px 0"}}>
+        {product.map((products, i) => (
+          <div key={i}>
+            <Card name={products.name} description={products.description} price={products.price} img={products.img}/>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
