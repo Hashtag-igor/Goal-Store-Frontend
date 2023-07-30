947aab83cc1f7ec922668b571c87d735e9c637e0
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-07-29 22:36:10 -0300

    Adicionei a logica de clicar em um card de camisas e ser enviado para a ProfilePage dessa camisa clicada, nas pages: BRShirts, NationShirts, RetroShirts e RDMShirts

--- src/pages/BRShirts.jsx
+++ src/pages/BRShirts.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Card from "../components/Card"
 
 export default function BRShirts() {
   const [BRShirts, setBRShirts] = useState([]);
+
+  const navigate = useNavigate()
 
   // Função para buscar as camisas da API e atualizar o estado
   function buscarCamisas() {
@@ -15,6 +18,10 @@ export default function BRShirts() {
         console.error('Erro ao obter os dados da API:', error);
       });
   }
+
+  const goToTheProfilePage = (product) => {
+    navigate("/profile", { state: { product } });
+  };
 
   // Utilize useEffect para chamar a função buscarCamisas ao carregar o componente
   useEffect(() => {
@@ -37,7 +44,7 @@ export default function BRShirts() {
       <h1>BRShirts</h1>
       <div style={{display: "flex", justifyContent: "space-evenly", flexWrap: "wrap", gap: "30px 0px"}}>
         {camisasFiltradas.map((shirts, i) => (
-          <div key={i}>
+          <div key={i} onClick={() => goToTheProfilePage(shirts)}>
             <Card name={shirts.name} description={shirts.description} price={shirts.price} img={shirts.img}/>
           </div>
         ))}
