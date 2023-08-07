0370731bdc876c19dd149fcecec78b4f9651dd57
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-08-07 14:27:21 -0300

    Adicionei o styled-components ao arquivo SearchPage

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,6 +1,40 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Card from "../components/Card";
+import { MapWrapper } from "../styles/SharedFiles"
+import { styled } from "styled-components";
+
+
+const MapContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  gap: 30px 0;
+  width: 100%;
+`
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  align-items: center;
+  gap: 50px 0;
+  margin: 40px auto;
+`
+const SearchTitle = styled.h2`
+  width: 95%;
+  font-size: 34px;
+  font-family: 'Inter', sans-serif;
+  font-family: 'Oswald', sans-serif;
+`
+const SearchTitleResults = styled.span`
+  font-family: 'Inter', sans-serif;
+  font-family: 'Oswald', sans-serif;
+  color: #0c441c;
+  font-weight: bolder;
+  font-size: 42px;
+`
+
 
 export default function SearchPage() {
   const [searchProducts, setSearchProducts] = useState([]);
@@ -28,19 +62,16 @@ export default function SearchPage() {
   }, [searchTerm, searchProducts]);
 
   return (
-    <div>
-      <h1>Resultados para: {searchTerm}</h1>
-      {searchProducts.map((products, i) => (
-        <div key={i} onClick={() => goToTheProfilePage(products)}>
-          <Card
-            name={products.name}
-            description={products.description}
-            price={products.price}
-            img={products.img}
-          />
-        </div>
-      ))}
-    </div>
+    <SearchContainer>
+      <SearchTitle>Resultados para: <SearchTitleResults>{searchTerm}</SearchTitleResults></SearchTitle>
+      <MapContainer>
+        {searchProducts.map((products, i) => (
+          <MapWrapper key={i} onClick={() => goToTheProfilePage(products)}>
+            <Card name={products.name} description={products.description} price={products.price} img={products.img}/>
+          </MapWrapper>
+        ))}
+      </MapContainer>
+    </SearchContainer>   
   );
 }
 
