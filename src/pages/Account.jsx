94f1fb7038ad38c68470577ccf99a5388392770e
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-08-18 13:42:29 -0300

    Adicionei a estilização da AccountPage para a versão de Desktop

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,6 +1,44 @@
-import React, { useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { UserContext } from '../contexts/UserContext';
 import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+import Links from '../components/Links'
+
+
+const AccountContainer = styled.div`
+  display: flex;
+  flex-direction: column; 
+  align-items: center; 
+  gap: 25px 0; 
+  margin: 70px auto 100px auto;
+`
+const AccountTitle = styled.h2`
+  font-size: 40px;
+`
+const AccountButton = styled.button`
+  background-color: #333745;
+  color: white;
+  padding: 15px 20px;
+  font-size: 18px;
+  font-weight: bolder;
+  border: 1px solid #333745;
+
+  &:hover {
+    background-color: #fff;
+    color: #333745;
+    cursor: pointer;
+  }
+`
+const BeStrong = styled.strong`
+  padding-left: 10px;
+`
+const AccountInfo = styled.p`
+  font-size: 20px;
+`
+const AccountWarning = styled.p`
+  margin-top: 60px;
+`
+
 
 export default function Account() {
   const { userLoggedIn, updateUserLoggedIn } = useContext(UserContext);
@@ -20,17 +58,21 @@ export default function Account() {
   };
 
   return (
-    <div>
+    <>
+      <Links />
       {userLoggedIn ? (
-        <div>
-          <h1>Bem-Vindo, {userLoggedIn.name}!</h1>
-          <p>Email: {userLoggedIn.email}</p>
-          <p>Senha: {userLoggedIn.password}</p>
-          <button onClick={handleLogout}>Sair</button> {/* Botão para deslogar */}
-        </div>
+        <AccountContainer>
+          <AccountTitle>Bem-Vindo, {userLoggedIn.name}!</AccountTitle>
+          <AccountInfo>Seu Email: <BeStrong>{userLoggedIn.email}</BeStrong></AccountInfo>
+          <AccountInfo>Sua Senha: <BeStrong>{userLoggedIn.password}</BeStrong></AccountInfo>
+          <AccountButton onClick={handleLogout}>Deslogar</AccountButton>
+          <AccountWarning>*Em breve você poderá alterar a senha do seu usuário..</AccountWarning>
+        </AccountContainer>
       ) : null}
-    </div>
+    </>
   );
 }
+
+
 
 
